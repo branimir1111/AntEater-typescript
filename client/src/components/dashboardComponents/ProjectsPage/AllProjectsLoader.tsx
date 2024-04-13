4d9b2beba9abbe8846164f734b8b61f6ad2fe1d1
unknown <branimir1111@gmail.com>
2024-04-13 22:24:07 +0200

    refactoring loader

--- client/src/components/dashboardComponents/ProjectsPage/AllProjectsLoader.tsx
+++ client/src/components/dashboardComponents/ProjectsPage/AllProjectsLoader.tsx
@@ -4,6 +4,7 @@ const AllProjectsLoader = () => {
   return (
     <>
       <Skeleton className="w-[167px] h-10 rounded-sm m-4 mb-10" />
+      <Skeleton className="w=full h-24 rounded-sm m-4" />
       <div className="w-full grid place-items-center gap-4 md:grid-cols-2 2xl:grid-cols-3 p-4">
         <Skeleton className="w-full h-[356px] rounded-md" />
         <Skeleton className="w-full h-[356px] rounded-md" />
