521ed6f85dca475f0c285fcd87342f32eaac3b8f
unknown <branimir1111@gmail.com>
2024-05-02 21:51:25 +0200

    testing createTask controller

--- controllers/taskControllers/createTask.js
+++ controllers/taskControllers/createTask.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import TaskModel from '../../models/taskModel.js';
 
 const createTask = async (req, res) => {
+  console.log(req.body);
   res.status(StatusCodes.OK).json({ msg: 'This is createTask controller!' });
 };
 
