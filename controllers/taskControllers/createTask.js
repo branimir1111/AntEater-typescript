7bf4ec93119995156edba89b7479308ff3c9f254
unknown <branimir1111@gmail.com>
2024-05-03 21:56:38 +0200

    createTask model

--- controllers/taskControllers/createTask.js
+++ controllers/taskControllers/createTask.js
@@ -3,7 +3,6 @@ import TaskModel from '../../models/taskModel.js';
 
 const createTask = async (req, res) => {
   console.log(req.body);
-  console.log(req.body);
   res.status(StatusCodes.OK).json({ msg: 'This is createTask controller!' });
 };
 
