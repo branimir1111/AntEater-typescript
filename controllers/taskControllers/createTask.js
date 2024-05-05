76bdb2df66325b8fcd20f78b49a89089c08b9b14
unknown <branimir1111@gmail.com>
2024-05-05 20:40:12 +0200

    newTaskCreated

--- controllers/taskControllers/createTask.js
+++ controllers/taskControllers/createTask.js
@@ -3,7 +3,7 @@ import TaskModel from '../../models/taskModel.js';
 
 const createTask = async (req, res) => {
   const newTaskCreated = await TaskModel.create(req.body);
-
+  console.log(newTaskCreated);
   res
     .status(StatusCodes.OK)
     .json({ msg: 'Task successfully created!', newTaskCreated });
