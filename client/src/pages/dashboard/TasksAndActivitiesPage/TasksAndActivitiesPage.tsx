e0c81af61677e9b55d9393c41a0dca5edcd26ed8
unknown <branimir1111@gmail.com>
2024-05-07 22:25:56 +0200

    installing new components

--- client/src/pages/dashboard/TasksAndActivitiesPage/TasksAndActivitiesPage.tsx
+++ client/src/pages/dashboard/TasksAndActivitiesPage/TasksAndActivitiesPage.tsx
@@ -2,11 +2,7 @@ import {
   TasksAndActivitiesFilter,
   TasksAndActivitiesContainer,
 } from '@/components';
-import {
-  customFetch,
-  type ParamsData,
-  type AllProjectsResponseWithParams,
-} from '@/utils';
+import { customFetch } from '@/utils';
 
 import { useQuery } from '@tanstack/react-query';
 
@@ -21,7 +17,7 @@ const allProjectsQuery = () => {
   const { search, status, sort, limit, page } = params;
   return {
     queryKey: [
-      'projects',
+      'projectsForTasks',
       search ?? '',
       status ?? 'all',
       sort ?? 'newest',
@@ -44,12 +40,12 @@ const TasksAndActivitiesPage = () => {
   if (isError) {
     return <h1>Error...</h1>;
   }
-  console.log(data.allProjects);
+  const projectsList = data.allProjects;
 
   return (
     <section className="w-full outlet-hight p-8 bg-background-first">
       <h1 className="text-3xl">TasksAndActivitiesPage</h1>
-      <TasksAndActivitiesFilter />
+      <TasksAndActivitiesFilter projectsList={projectsList} />
       <TasksAndActivitiesContainer />
     </section>
   );
