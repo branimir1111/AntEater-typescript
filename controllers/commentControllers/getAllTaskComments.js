27f1658b27d491631dadff1b922bf068954fb879
unknown <branimir1111@gmail.com>
2024-05-24 13:25:25 +0200

    finished getAllTaskComments controller

--- controllers/commentControllers/getAllTaskComments.js
+++ controllers/commentControllers/getAllTaskComments.js
@@ -1,9 +1,14 @@
 import { StatusCodes } from 'http-status-codes';
 import CommentTaskModel from '../../models/commentTaskModel.js';
+import { userProjectAndTaskFromTaskComment } from '../../utils/aggregations.js';
 
 const getAllTaskComments = async (req, res) => {
-  res
-    .status(StatusCodes.OK)
-    .json({ msg: 'This is getAllTaskCOMMENTS controller!' });
+  const allTaskComments = await CommentTaskModel.aggregate([
+    ...userProjectAndTaskFromTaskComment,
+  ]);
+
+  const numOfTaskComments = allTaskComments.length;
+
+  res.status(StatusCodes.OK).json({ numOfTaskComments, allTaskComments });
 };
 export { getAllTaskComments };
