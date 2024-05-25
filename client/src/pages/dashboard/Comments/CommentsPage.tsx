1af5685961d05ee5fd835ba3470c143eed88a8ca
unknown <branimir1111@gmail.com>
2024-05-25 22:02:47 +0200

    start working on comments page

--- client/src/pages/dashboard/Comments/CommentsPage.tsx
+++ client/src/pages/dashboard/Comments/CommentsPage.tsx
@@ -1,4 +1,8 @@
 const CommentsPage = () => {
-  return <div className="text-lg">CommentsPage</div>;
+  return (
+    <section className="w-full outlet-hight p-4 bg-background-first">
+      <h1 className="text-3xl">CommentsPage</h1>
+    </section>
+  );
 };
 export default CommentsPage;
