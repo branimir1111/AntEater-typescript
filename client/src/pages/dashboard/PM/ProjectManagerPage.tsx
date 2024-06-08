c0e2de5582cdba3f5d1e82eac4446ea77e6186ee
unknown <branimir1111@gmail.com>
2024-06-08 20:54:48 +0200

    refactor PM projects page

--- client/src/pages/dashboard/PM/ProjectManagerPage.tsx
+++ client/src/pages/dashboard/PM/ProjectManagerPage.tsx
@@ -1,7 +1,7 @@
 const ProjectManagerPage = () => {
   return (
     <section className="w-full outlet-hight p-4 bg-background-first">
-      <h1 className="text-3xl">ProjectManagerPage (MyProjects)</h1>
+      <h1 className="text-3xl">PM Projects</h1>
     </section>
   );
 };
