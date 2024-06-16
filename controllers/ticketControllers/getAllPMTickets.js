a14599e775c95bf2022f80888c4097571dc00e45
Branimir Djordjevic <branimir1111@gmail.com>
2024-06-16 09:37:13 +0200

    finished getAllPMTickets controller and add status to editPMTask

--- controllers/ticketControllers/getAllPMTickets.js
+++ controllers/ticketControllers/getAllPMTickets.js
@@ -1,8 +1,112 @@
 import { StatusCodes } from 'http-status-codes';
+import TicketModel from '../../models/ticketModel.js';
+import mongoose from 'mongoose';
 
 const getAllPMTickets = async (req, res) => {
-  res
-    .status(StatusCodes.OK)
-    .json({ msg: 'This is getAllPMTickets controller' });
+  const projectManagerId = req.user.userId;
+  const { page: currPage } = req.query;
+
+  const limit = 5;
+  const page = Number(currPage) || 1;
+  const skip = (page - 1) * limit;
+
+  const allPMTickets = await TicketModel.aggregate([
+    {
+      $lookup: {
+        from: 'projects',
+        localField: 'projectId',
+        foreignField: '_id',
+        as: 'project',
+      },
+    },
+    {
+      $unwind: {
+        path: '$project',
+        preserveNullAndEmptyArrays: false,
+      },
+    },
+    {
+      $match: {
+        'project.projectManager': new mongoose.Types.ObjectId(projectManagerId),
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'assignedTo',
+        foreignField: '_id',
+        as: 'assignedUser',
+      },
+    },
+    {
+      $unwind: {
+        path: '$assignedUser',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        title: 1,
+        description: 1,
+        assignedTo: {
+          _id: '$assignedUser._id',
+          firstName: '$assignedUser.firstName',
+          lastName: '$assignedUser.lastName',
+          avatar: '$assignedUser.avatar',
+        },
+        ticketType: 1,
+        priority: 1,
+        status: 1,
+        project: {
+          _id: '$project._id',
+          projectName: '$project.projectName',
+        },
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+  ]);
+
+  const totalCountResult = await TicketModel.aggregate([
+    {
+      $lookup: {
+        from: 'projects',
+        localField: 'projectId',
+        foreignField: '_id',
+        as: 'project',
+      },
+    },
+    {
+      $unwind: {
+        path: '$project',
+        preserveNullAndEmptyArrays: false,
+      },
+    },
+    {
+      $match: {
+        'project.projectManager': new mongoose.Types.ObjectId(projectManagerId),
+      },
+    },
+    { $count: 'totalTickets' },
+  ]);
+
+  const numOfFilteredTickets =
+    totalCountResult.length > 0 ? totalCountResult[0].totalTickets : 0;
+
+  const numOfPages = Math.ceil(numOfFilteredTickets / limit);
+
+  res.status(StatusCodes.OK).json({
+    numOfPMTickets: numOfFilteredTickets,
+    numOfPages,
+    currentPage: page,
+    allPMTickets,
+  });
 };
 export { getAllPMTickets };
