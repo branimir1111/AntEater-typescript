4e7f6f26d74477c54253abc5eb05f9bda94915cc
Branimir Djordjevic <branimir1111@gmail.com>
2024-07-01 09:15:42 +0200

    finished gelAllTickets controller for admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getAllProjectsAdmin } from '../controllers/adminControllers/adminProjects/getAllProjectsAdmin.js';
-import { getAllTasksAdmin } from '../controllers/adminControllers/adminProjects/getAllTasksAdmin.js';
+import { getAllProjectsAdmin } from '../controllers/adminControllers/getAllProjectsAdmin.js';
+import { getAllTasksAdmin } from '../controllers/adminControllers/getAllTasksAdmin.js';
+import { getAllTicketsAdmin } from '../controllers/adminControllers/getAllTicketsAdmin.js';
 
 router.route('/all-admin-projects').get(getAllProjectsAdmin);
 router.route('/all-admin-tasks').get(getAllTasksAdmin);
+router.route('/all-admin-tickets').get(getAllTicketsAdmin);
 
 export default router;
