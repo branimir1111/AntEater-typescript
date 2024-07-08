d68e85d3c0e360d391c4a66229bbc9d834ecc266
Branimir Djordjevic <branimir1111@gmail.com>
2024-07-08 17:37:13 +0200

    completed getTicketStats controller

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 const router = Router();
 import { getProjectsStats } from '../controllers/statisticsControllers/projectsStats.js';
 import { getTasksStats } from '../controllers/statisticsControllers/tasksStats.js';
+import { getTicketsStats } from '../controllers/statisticsControllers/ticketsStats.js';
 
 router.route('/projects-stats').get(getProjectsStats);
 router.route('/tasks-stats').get(getTasksStats);
+router.route('/tickets-stats').get(getTicketsStats);
 
 export default router;
